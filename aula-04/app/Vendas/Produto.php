c0b1f8263b8cf9e58916a0d3096e63bd5848b205
William Correa <wilcorrea@gmail.com>
2023-06-10 18:41:39 -0300

    aula-04

--- aula-04/app/Vendas/Produto.php
+++ aula-04/app/Vendas/Produto.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Main;
+namespace App\Vendas;
 
 class Produto
 {
--- aula-04/index.php
+++ aula-04/index.php
@@ -1,14 +1,16 @@
 <?php
 
-$cliente = new App\Main\Cliente('William');
+require __DIR__ . '/vendor/autoload.php';
 
-$pedido = new App\Main\Pedido($cliente);
+$cliente = new App\Vendas\Cliente('William');
+
+$pedido = new App\Vendas\Pedido($cliente);
 
 $pedido->adicionarItem(
-    new App\Main\PedidoItem(new App\Main\Produto('Salgado', 3.45), 2)
+    new App\Vendas\PedidoItem(new App\Vendas\Produto('Salgado', 3.45), 2)
 );
 $pedido->adicionarItem(
-    new App\Main\PedidoItem(new App\Main\Produto('Sabonete', 4.35), 3)
+    new App\Vendas\PedidoItem(new App\Vendas\Produto('Sabonete', 4.35), 3)
 );
 
 echo json_encode($pedido, JSON_PRETTY_PRINT);
